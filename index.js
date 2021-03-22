eaae738710a99b34a4ac63c317c34cab320b6f50
AClappers <80983762+AClappers@users.noreply.github.com>
2021-03-22 19:19:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,170 +1,117 @@
-const { Client } = require('discord.js')
+//Require
+const { Client, TeamMember } = require('discord.js')
 
 const client = new Client({
     partials: ['GUILD_MEMBER', 'CHANNEL', 'USER', 'REACTION']
 })
+
+//vars
 let mode = 0;
 let gif = ["https://tenor.com/view/plague-doctor-dance-gif-19130639", "https://tenor.com/view/he-hehe-boy-boi-boyi-gif-7890844", "https://tenor.com/view/why-do-you-cum-cum-cyberpunk-gif-19751361", "https://tenor.com/view/dead-cat-cpr-funny-animals-cute-revive-gif-13712625"];
 let loop = true;
 let default_id = "754049600599621677";
+let default_role ="823615437894844416";
 let msg;
+let msg_old;
 let maicC;
+let prfix = "Alain";
+let args;
+//funct
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-function reload(){
-    loop=true;
+
+function cWordCheck(tab, msg){
+    let cWord = false;
+    let i = 0;
+    while(cWord===false && i < tab.length)
+    {
+        if(tab[i] == "COQUINE" && msg.deletable)
+        {
+            cWord=true;
+        }
+        i++;
+    }
+    return cWord;
 }
 
-client.on('ready', () => {
 
+
+//On start
+client.on('ready', () => {
+    prefix = prfix.toUpperCase();
     console.log('Le bot est connecté !')
-    let prfix = "Alain";
 
     client.user.setActivity('Fortnite', {
         type: 'PLAYING'
     })
 
+//On message
     client.on('message', (message) => {
-        let argse = message.content.split(" ");
-        for(let i = 0 ; i < argse.length ; i++)
+        msg_content = message.content.toUpperCase();
+        if(msg_content.startsWith(prefix))
         {
-            if(argse[i].toUpperCase() === "COQUINE" && message.deletable)
+            args = msg_content.split(" ");
+            
+            if(cWordCheck(args, message) && msg_old!=message.id)
             {
-                message.delete();
-            }
-        }
-        if(message.channel.type !="dm")
-        {
-             mainC = message.guild.channels.cache.get(default_id);
-
-        }
-        else
-        {
-            if(!message.author.bot && msg!=undefined)
-            {
-                msg.channel.send("De "+message.author.username+"  : "+message.content);
-
-            }
-        }
-            if(message.author.username != "Eurêka" )
-            {
-                if(mode===1 && loop)
+                msg_old = message.id;
+                if(message.channel.type !="dm")
                 {
-                    message.reply("Tocard");
-                    loop=false;
-
+                     mainC = message.guild.channels.cache.get(default_id);
+                }
+                else
+                {
+                    if(!message.author.bot && msg!=undefined)
+                    {
+                        msg.channel.send("De "+message.author.username+"  : "+message.content);
+                    }
                 }
 
-            }   
-
-
-            if(mode!=2)
-            {
-                if(message.content === prfix+", GIF"&& loop)
+                if(args[2]==="?")
                 {
-                    let rdm = getRandomInt(gif.length);
-                    message.reply(gif[rdm]);
-                    loop=false;
-
+                    message.channel.send("ici") //HERE
                 }
-                if(message.content === prfix+"?"&& loop)
+                if(message.channel.type!="dm")
                 {
-                    message.reply("Ici");
-                    loop=false;
-
-                }
-                if((message.content.toUpperCase() === "CIMER "+prfix.toUpperCase() || message.content.toUpperCase() === "MRC "+prfix.toUpperCase() || message.content.toUpperCase() === "MERCI "+prfix.toUpperCase() ) && loop)
-                {
-                    message.channel.send("Tkt");
-
-                    loop=false;
-
-                }
-            }
-            if(message.author.username === "Eurêka")
-            {
-                    if(message.content === prfix+", qui est ton guide?" && loop)
+                    if(!message.author.member.roles.get(default_role) && mode === 1)
                     {
-                        message.channel.send("Toi  :pray:");
-                        loop=false;
-
-                    }
-                    if(message.content === prfix+", pourquoi es-tu nul?" && loop)
+                            message.reply("Tocard"); //HERE
+                    } 
+                    if(message.author.member.roles.get(default_role))
                     {
-                        message.channel.send("Demande à David :snake:");
-                        loop=false;
-
-                    }
-                    if(message.content === prfix+", mode 1" && loop)
-                    {
-                        message.channel.send("Oké ! :nerd:");
-                        mode=1;
-                        loop=false;
-
-                    }
-                    if(message.content === prfix+", mode 2" && loop)
-                    {
-                        message.channel.send("Mode 2, reçu");
-                        mode=2;
-                        loop=false;
-
-                    }
-                    if(message.content === prfix+", mode 0" && loop)
-                    {
-                        message.channel.send("Fait!");
-                        mode=0;
-                        loop=false;
-                    }
-                    if(message.content === prfix+", t'es en quel mode?"&& loop )
-                    {
-                        message.channel.send("Euh att bg...");
-                        message.channel.send("Mode "+mode+" bg");
-                        loop=false;
-                    }
-                    if(message.content === prfix+", id salon stp"&& loop )
-                    {
-                        message.channel.send("L'id du salon est : "+message.channel.id);
-
-                        loop=false;
-                    }
-                    if(message.content === prfix+", mode 3"&& loop )
-                    {
-                        message.channel.send("Mode 3 activé");
-                        mode=3;
-                        loop=false;
-                    }
-                    if(message.content === prfix+", stop mode 3"&& loop )
-                    {
-                        message.channel.send("Mode 3 désactivé");
-                        mode=0;
-                        loop=false;
-                    }
-                    if(message.content === prfix+", change default_id"&& loop )
-                    {
-                        message.channel.send("Changé :cowboy:");
-                        default_id = message.channel.id;
-                        mainC = message.guild.channels.cache.get(default_id);
-                        loop=false;
-                    }
-                    if(mode===3 && message.channel.type != "dm" &&  mainC!=undefined)
-                    {
-                        if(message.channel.id != mainC && message.content != prfix+", stop mode 3" && loop )
+                        if(args[2]==="MODE" && (args[3]=="0" || args[3]=="1" || args[3]=="2"))
                         {
-                            mainC.send(message);
+                            mode = parseInt(args[3]);
+                            message.channel.send("Fait"); //HERE
+
                         }
-                 
-                    }
-                    if(message.content.startsWith(prfix+", dm "))
-                    {
+                        if(args[2]==="MODE" && args[3]==="?")
+                        {
+                            message.channel.send("Mode = "+mode);
+
+                        }
+                        if(mode===2 && mainC!=undefined)
+                        {
+                            if(message.channel.id != mainC && args[2]==="STOP")
+                            {
+                                mainC.send(message.content);
+                            }
+                     
+                        }
+                        if(args[2]==="CHANGE" && args[3]==="DEFAULT_ID")
+                        {
+                            message.channel.send("Changé :cowboy:"); //HERE
+                            default_id = message.channel.id;
+                            mainC = message.guild.channels.cache.get(default_id);
+                        }
+                        if(args[2]==="DM" && mode !=2)
+                        {
                         msg=message;
                         message.channel.send("Ok");
                         let mention = message.mentions.members.first();
                         if(mention != undefined)
                         {
-
-                            let args = message.content.split(" ");
-                            let str = "";
                             for(let i = 3 ; i < args.length ; i++)
                             {
                                 str+=" "+args[i];
@@ -175,14 +122,18 @@ client.on('ready', () => {
                             }) ;
 
                         } 
+                        }
+
+
                     }
-                    
-                }  
-                setTimeout(reload, 1000);
-            
+                }
+                 }
+            else{
+                message.delete();
+            }
+        } 
     })
 
-})
+    })
+    client.login(process.env.TOKEN);
 
-
-client.login(process.env.TOKEN);
