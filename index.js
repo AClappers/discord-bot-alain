c473693c5772d14ac608158272828c1fcc4d7211
AClappers <80983762+AClappers@users.noreply.github.com>
2021-03-22 21:12:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on('ready', () => {
             
             message.delete();
         }
-        if(mode===2 && mainC!=undefined && usr_md === message.author.id)
+        if(mode===2 && mainC!=undefined && usr_md === message.author.id && msg_content!="ALAIN MODE 2")
         {
             if(message.channel.id != mainC && args[2]!="STOP")
             {
